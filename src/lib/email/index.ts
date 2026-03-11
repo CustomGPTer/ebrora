f47f1c1ab2ce8f8d2d5f6590ffc1543eef8b02f0
CustomGPTer <jacksonja21@gmail.com>
2026-03-11 18:42:49 +0000

    Fix: add sendSubscriptionConfirmationEmail to email module

--- src/lib/email/index.ts
+++ src/lib/email/index.ts
@@ -1,10 +1,21 @@
 export { sendEmail } from './send-email';
 export {
-  welcomeEmail,
-  verificationEmail,
-  ramsCompleteEmail,
-  ramsFailedEmail,
-  subscriptionConfirmEmail,
-  usageLimitWarningEmail,
-  passwordResetEmail,
+    welcomeEmail,
+    verificationEmail,
+    ramsCompleteEmail,
+    ramsFailedEmail,
+    subscriptionConfirmEmail,
+    usageLimitWarningEmail,
+    passwordResetEmail,
 } from './templates';
+
+import { sendEmail } from './send-email';
+import { subscriptionConfirmEmail } from './templates';
+
+export async function sendSubscriptionConfirmationEmail(
+    email: string,
+    tier: string
+  ): Promise<boolean> {
+    const { subject, html } = subscriptionConfirmEmail('Customer', tier, '');
+    return sendEmail(email, subject, html);
+}
