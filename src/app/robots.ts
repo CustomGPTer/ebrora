0a759dd07611884015b35774284777eb274ff903
CustomGPTer <jacksonja21@gmail.com>
2026-03-21 17:17:01 +0000

    fix(seo): update sitemap URL to www.ebrora.com in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/']
     },
-    sitemap: 'https://ebrora.com/sitemap.xml'
+    sitemap: 'https://www.ebrora.com/sitemap.xml'
   };
 }
